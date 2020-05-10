624c39babdfdab99820a4d58338f50853f970ce8
ninjee <yninjbadgar@gmail.com>
2020-05-10 20:57:13 +0800

    Financial website finish

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function initMap() {
+  const loc = { lat: 47.918585, lng: 106.917779 };
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc,
+  });
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+// 47.918585,106.917779
